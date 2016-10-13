5fcafbfd0d32226c89855dd744e7dcca073448f3
Alessandro Holanda <alessandro.holanda@hotmail.com>
2016-10-13 08:57:31 -0300

    BPM-1 adicionado visualizador de diagramas BPM na ui principal

--- ui/client/src/App.js
+++ ui/client/src/App.js
@@ -10,7 +10,18 @@ class App extends Component {
 
   render() {
     return(
-      <Viewport diagram={myProcess} height={800} />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      
+        <Viewport diagram={myProcess} height={800} />
+      </div>
     );
   }
 }
