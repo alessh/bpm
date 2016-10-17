6d68768e19928588ed0d467ccde0d6193f50b178
Alessandro Holanda <alessandro.holanda@hotmail.com>
2016-10-17 11:26:46 -0200

    BPM-27 botão iniciar processo

--- ui/client/src/task/ProcessPanel.js
+++ ui/client/src/task/ProcessPanel.js
@@ -7,7 +7,16 @@ import processUseCase from './../test/use_case/versao-0.1.0/process.json';
 import executionUseCase from './../test/use_case/versao-0.1.0/execution.json';
 
 export default class ProcessPanel extends Component {
-	
+	constructor(props) {
+		super(props);
+
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onClick(p) {
+		alert('Inicia o processo: ' + p.name)
+	}
+
 	render() {
 		const style = {
 	      viewport: {
@@ -28,10 +37,29 @@ export default class ProcessPanel extends Component {
 
 		return (
 			<div>
-				<h1 style={{textAlign: 'left', margin: 20}} >{process.name}</h1>
+				<div style={{display: 'flex',textAlign: 'left'}} > 
+
+					<div style={{width: '100%', display: 'block', }} >
+						<h1 style={{margin: 20}} >{process.name}</h1>
+						<span style={{margin: 20, fontSize: '1.5em'}} >{process.description}</span>
+					</div>
+
+					<input 
+						type="button" 
+						value="iniciar" 
+						style={{
+							height: 80,
+							width: 80, 
+							right: 0, 
+							border: 1, 
+							borderRadius: '50%',
+							margin: 20
+						}} 
+						onClick={this.onClick.bind(this, process)}
+					/>
+				</div>
+
 				
-				<line />
-				<h5 style={{textAlign: 'left', margin: 20}} >{process.description}</h5>
 
 				<Viewport style={style.viewport} diagram={processUseCase} height={400} />
 
